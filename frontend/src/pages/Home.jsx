f1b8ad112ab6f08ffd3f4b1d7bc7ecce080c8d6e
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-06-25 15:31:12 +0530

    Add Home page to display product listing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import ProductItem from '../components/ProductItem.js';
+
+
+function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      const fetchProducts = async () => {
+        const res = await axios.get('http://localhost:5000/api/products');
+        setProducts(res.data);
+      };
+      fetchProducts();
+    }, []);
+    
+  return (
+    <div>
+      <h2>Products</h2>
+      <div  style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
+        {products.map((product) => (
+            <ProductItem key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
