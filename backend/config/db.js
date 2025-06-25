867616c14c4a646a76b18cd35d255f378d2be88c
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-06-25 14:17:17 +0530

    Somr changes in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(' mongodb://127.0.0.1:27017/ecommerce');
+        await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
         console.log('MongoDB connected');
         
     } catch (error) {
